0659cd7f8690e083bfd7ede9b7df9afa600a09b6
henrikbw <henrik.wisting@yahoo.com>
2024-05-26 12:14:15 +0200

    marketing channels

--- models/marketing_channels.sql
+++ models/marketing_channels.sql
@@ -23,7 +23,7 @@ joined as (
         attributed_marketing_channel.marketing_channel,
         order.amount,
         marketing_channel_cost.marketing_channel_cost,
-        order.amount - marketing_channel_cost.marketing_channel_cost as marketing_profit
+        order.amount - marketing_channel_cost.marketing_channel_cost as marketing_profit -- assuming marketing_channel_cost is per order
     from orders
     left join attributed_marketing_channel
         on orders.order_id = attributed_marketing_channel.order_id
@@ -33,16 +33,21 @@ joined as (
 
 final as (
     select
-        year,
+        year, -- not necessary as every order is 2018, but can be grouped to give an impression about the channels development
         marketing_channel,
-        count(distinct order_id) as order_count,
-        sum(amount) as total_order_amount,
-        sum(marketing_channel_cost) as total_marketing_cost,
-        sum(marketing_profit) as total_marketing_profit,
-        avg(markering_profit) as avg_marketing_profit
+        count(distinct order_id) as orders_count, -- number of orders achieved per channel
+        count(distinct customer_id) as customers_count -- number of distinct customers per channel
+        sum(amount) as total_order_amount, -- revenue per channel 
+        sum(marketing_channel_cost) as total_marketing_cost, -- marketing cost per channel
+        sum(marketing_profit) as total_marketing_profit, -- total profit when marketing cost is subtracted per channel
+        avg(markering_profit) as avg_marketing_profit -- avg marketing profit per channel
 
     from joined
     group by year, marketing_channel
 )
+
+-- assuming marketing channel cost is per sale
+-- other options
+-- status set to completed - not necessary as a return is assumed to say more about the products than the sale
 
 select * from final
